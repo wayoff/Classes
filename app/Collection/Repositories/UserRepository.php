b3899f6e685aff991e43ae2f88412f21d43f2bf0
Aldikun17 <romualdo.ducon@gmail.com>
2017-06-02 13:34:07 +0800

    still working

--- app/Collection/Repositories/UserRepository.php
+++ app/Collection/Repositories/UserRepository.php
@@ -3,9 +3,110 @@
 class UserRepository
 {
 	
-	function __construct()
+	protected $person = [];
+
+	function __construct($person)
 	{
-		die('Hello.');	
+		$this->persons = $person;
+	}
+
+	public function all()
+	{
+		return $this->persons;
+	}
+
+	public function where($key,$value)
+	{
+		foreach($this->persons as $person)
+		{
+			if($person[$key] == $value)
+			{
+				return $person;
+			}
+		}
+		return null;
+	}
+
+	public function getLastId()
+	{
+
+		$lastId;
+
+		if(!empty($this->persons))
+		{
+
+			foreach($this->persons as $person)
+			{
+				$lastId = $person['id'];
+			}
+			return $lastId;
+			
+		}
+		$lastId = 0;
+		return $lastId;
+	}
+
+	public function update($key,$value)
+	{
+		foreach($this->persons as $person)
+		{
+			if($person[$key] == $value)
+			{
+				$person['first_name'] = 'Charlotte';
+				$person['last_name'] = 'Fheir';
+				
+				return $this->persons;
+			}
+		}
+		return null;
+	}
+
+	public function delete($key,$value)
+	{
+
+		foreach($this->persons as $person)
+		{
+
+			if($person[$key] == $value)
+			{
+
+				$deleted = array_splice($person, 0,0);
+				return $this->persons;
+			}
+
+		}
+		return null;
+	}
+
+	public function create($creation)
+	{
+		$add = array_push($this->persons,$creation);
+		return $this->persons;
+	}
+
+	public function sortAscending($key)
+	{
+		$sortAsc = [];
+
+		if($key == 'first_name')
+		{
+
+			sort($this->persons);
+			foreach($this->persons as $sort)
+			{	
+				return $this->persons;
+
+			}
+			return null;
+
+		}
+
+		krsort($this->persons);
+		foreach($this->persons as $rsort)
+		{
+			return $this->persons;
+		}
+	
 	}
 
 }
--- index.php
+++ index.php
@@ -2,5 +2,58 @@
 use Collection\Repositories\UserRepository as UserRepository;
 use Collection\Filters\AuthFilter as AuthFilter;
 require_once 'app/start.php';
+require_once 'sample.php';
 
-$filter = new AuthFilter();
+$user = new UserRepository($people);
+//var_dump($user->all());
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div>
+		<form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+			<div>
+				<input type="text" name="last_id" value="<?= $user->getLastId() ?>" style="display:none"/>
+			</div>
+			<div>
+				<label>First Name: </label>
+				<input type="text" name="first_name" />
+			</div>
+			<div>
+				<label>Last Name: </label>
+				<input type="text" name="last_name" />
+			</div>
+			<div>
+				<label>Country: </label>
+				<input type="text" name="country" />
+			</div>
+			<div>
+				<label>Alias: </label>
+				<input type="text" name="alias" />
+			</div>
+			<div>
+				<input type="submit" name="submit" />
+			</div>
+		</form>
+	</div>
+</body>
+</html>
+<?php
+
+$create = [
+	'id' => @$_POST['last_id'] + 1,
+	'first_name' => @$_POST['first_name'],
+	'last_name' => @$_POST['last_name'],
+	'country' => @$_POST['country'],
+	'alias' => @$_POST['alias']
+];
+
+$created = $user->create($create);
+$where = $user->where('first_name','Ronwaldo');
+var_dump($where);
+echo "</br></br></br>";
+$all = $user->all();
+var_dump($all);
